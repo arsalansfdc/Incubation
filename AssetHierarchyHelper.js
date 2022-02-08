7c3fbd20467fe1c251f13845b66799cc47f8e990
arsalansfdc <98511157+arsalansfdc@users.noreply.github.com>
2022-02-08 14:17:28 +0530

    Update AssetHierarchyHelper.js

--- AssetHierarchyHelper.js
+++ AssetHierarchyHelper.js
@@ -1,5 +1,16 @@
 ({
-	helperMethod : function() {
-		
+	getAssets : function(component,event) {
+        var accountId = component.get("v.recordId");
+		var action = component.get("c.fetchAccountAssetHierarchy");
+        action.setParams({ accountId : accountId });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (component.isValid() && state === "SUCCESS") {
+                var assetList = response.getReturnValue();
+                assetList = JSON.parse(JSON.stringify(assetList).split('children').join('_children'));
+                component.set("v.gridData", assetList);
+            }          
+        });
+        $A.enqueueAction(action); 
 	}
 })
